8913b296df3532b810674bc01be9118d99796e15
Roxanna <rodrosera@gmail.com>
2026-09-17 13:21:20 +0200

    Add Hello Word program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    IO.println(String.format("Hello and welcome!"));
- // test  git
+public class Main {
+    public static void  main(String[]args){
+        System.out.println("Hello World");
+    }
 }
