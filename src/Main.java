899a0e0ee2bed642907b7b2946f374d1bb88e0c8
Roxanna <rodrosera@gmail.com>
2026-09-18 11:24:08 +0200

    Add procedural IoT logic for lamp and door in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void  main(String[]args){
-        System.out.println("Hello World");
+
+        int lightId = 1;
+        boolean isLightOn = false;
+        int brightness = 0;
+
+        String doorZone = "hall" ;
+        boolean isOpen = true;
+
+
+        if(isOpen) {
+            isLightOn = true;
+            brightness = 80;
+            System.out.println("LARM: dörrem är öppen!");
+            System.out.println("Lampstatus: " + brightness + " " + lightId + " " + isLightOn);
+        } else {
+            System.out.println("");
+        }
+        if(brightness >= 0 && brightness <= 100) {
+            System.out.println("Ljusstyrka är giltig: " + brightness);
+        }else {
+            System.out.println("Fel: ogiltig ljusstyrka!");
+        }
+
     }
 }
