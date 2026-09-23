de8125b6cbcf531d2e8b4e1dbb9dcc43dfbbc7a2
Roxanna <rodrosera@gmail.com>
2026-09-23 10:05:42 +0200

    Add dimmer loop, toggle and brightnessvalidation (level 2)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void  main(String[]args){
 
-
         int lightId = 101;
         boolean isLightOn = false;
         int brightness = 0;
@@ -24,6 +23,27 @@ public class Main {
             System.out.println("Fel: ogiltig ljusstyrka!");
         }
 
+        for (int i = 0 ; i <= 100 ; i += 20){
+            System.out.println((" Det aktuella dimmer är: " + i ));
+        }
 
+        System.out.println("Lampan är " + isLightOn);
+        isLightOn = !isLightOn;
+        System.out.println("Lampan är " + isLightOn);
+
+        brightness = 150;
+        if(brightness >= 0 && brightness <= 100) {
+            System.out.println("Ljusstyrka är giltig: " + brightness);
+        }else {
+            System.out.println("Fel: ogiltig ljusstyrka!");
+        }
+
+        brightness = -10;
+        if(brightness >= 0 && brightness <= 100) {
+            System.out.println("Ljusstyrka är giltig: " + brightness);
+        }else {
+            System.out.println("Fel: ogiltig ljusstyrka!");
+        }
     }
+
 }
