ef604b7f1edecdf67d680e57010e62059b6a89f3
Roxanna <rodrosera@gmail.com>
2026-09-23 12:10:31 +0200

    Refactor: move SmartLight test code to separate Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,15 @@
 public class Main {
-    public static void  main(String[]args){
+    public static void  main(String[]args) {
+        SmartLight lampa1 = new SmartLight("L1");
+        lampa1.turnOn();
+        lampa1.turnOff();
 
-        int lightId = 101;
-        boolean isLightOn = false;
-        int brightness = 0;
-
-        String doorZone = "hall" ;
-        boolean isOpen = true;
+        SmartLight lampa2 = new SmartLight("L2");
+        lampa2.turnOn();
+        lampa2.turnOff();
 
 
-        if(isOpen) {
-            isLightOn = true;
-            brightness = 80;
-            System.out.println("LARM: dörrem är öppen!");
-            System.out.println("Lampstatus: " + brightness + " " + lightId + " " + isLightOn);
-        } else {
-            System.out.println("");
-        }
-        if(brightness >= 0 && brightness <= 100) {
-            System.out.println("Ljusstyrka är giltig: " + brightness);
-        }else {
-            System.out.println("Fel: ogiltig ljusstyrka!");
-        }
-
-        for (int i = 0 ; i <= 100 ; i += 20){
-            System.out.println((" Det aktuella dimmer är: " + i ));
-        }
-
-        System.out.println("Lampan är " + isLightOn);
-        isLightOn = !isLightOn;
-        System.out.println("Lampan är " + isLightOn);
-
-        brightness = 150;
-        if(brightness >= 0 && brightness <= 100) {
-            System.out.println("Ljusstyrka är giltig: " + brightness);
-        }else {
-            System.out.println("Fel: ogiltig ljusstyrka!");
-        }
-
-        brightness = -10;
-        if(brightness >= 0 && brightness <= 100) {
-            System.out.println("Ljusstyrka är giltig: " + brightness);
-        }else {
-            System.out.println("Fel: ogiltig ljusstyrka!");
-        }
     }
 
 }
+
--- src/SmartLight.java
+++ src/SmartLight.java
@@ -18,13 +18,4 @@ public class SmartLight {
         isOn = false;
         System.out.println("Lampa nr " + deviceId + " är "  + isOn);
     }
-    public static void main(String[] args) {
-        SmartLight lampa1 = new SmartLight("L1");
-        lampa1.turnOn();
-        lampa1.turnOff();
-
-        SmartLight lampa2 = new SmartLight("L2");
-        lampa2.turnOn();
-        lampa2.turnOff();
-    }
 }
